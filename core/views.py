00819eb79d9f72c3f7ac5ec0fadbd9178f9edfe6
ebikdeli <ebikdeli@gmail.com>
2022-02-19 12:36:55 +0330

    02-19-2022 - 12:36 PM

--- core/views.py
+++ core/views.py
@@ -6,6 +6,8 @@ from .pgi import zarin_pay, zarin_verify
 # @login_required
 def pay(request):
     """First step of payment. This view redirect user to pgi to enter his/her ID cart"""
+    # This is the url we begin our payment proccedure. We need two arguments to begin payment:
+    # https://rapdana.herokuapp.com/api/pay?name=ehsan&order_id=4W8SSVQXES
     data = zarin_pay(request)
     if 'url' in data.keys():
         return redirect(data['url'])
