4e4f08f1702cd73483a5147fd6abfb7b1a8bce82
ebikdeli <ebikdeli@gmail.com>
2022-02-26 13:30:00 +0330

    02-26-2022 - 13:29 PM

--- rapadana/settings/production.py
+++ rapadana/settings/production.py
@@ -4,7 +4,9 @@ DEBUG = False
 
 # SECRET_KEY = 'django-insecure-7s4l(vt1vmpatobda4utligb+b)8cxncva4y$al+kf6(i2@b5s'   # WILL CHANGE
 
-ALLOWED_HOSTS = ['*']   # WILL CHANGE
+# # https://docs.djangoproject.com/en/4.0/ref/settings/#allowed-hosts
+# ALLOWED_HOSTS = ['*']   # WILL CHANGE
+ALLOWED_HOSTS = ['.rapadana.ir', ]
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
