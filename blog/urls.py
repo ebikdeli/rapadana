289ec5ff461093345d7487f255cdced3e7d3af1b
ebikdeli <ebikdeli@gmail.com>
2022-06-25 23:44:34 +0430

    html to pdf test added

--- blog/urls.py
+++ blog/urls.py
@@ -8,4 +8,6 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.BlogListView.as_view(), name='blog_list_view'),
+    path('pdf1/', views.GeneratePdf.as_view()),
+    path('pdf2/', views.render_pdf_view),
 ]
--- blog/views.py
+++ blog/views.py
@@ -21,3 +21,44 @@ class BlogDetalView(DetailView):
 
 class BlogCreate(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
     """Only authorized users can create new blog"""
+
+
+# importing the necessary libraries
+from django.http import HttpResponse
+from django.views.generic import View
+from .process import html_to_pdf 
+
+#Creating a class based view
+class GeneratePdf(View):
+     def get(self, request, *args, **kwargs):
+         
+        # getting the template
+        pdf = html_to_pdf('blog/templates/blog/blog_pdf_test.html')
+         
+         # rendering the template
+        return HttpResponse(pdf, content_type='application/pdf')
+
+
+# Another view to convert same html to pdf
+
+from django.template.loader import get_template
+from xhtml2pdf import pisa
+from .process import link_callback
+
+def render_pdf_view(request):
+    template_path = 'blog/templates/blog/blog_pdf_test.html'
+    context = {'myvar': 'this is your template context'}
+    # Create a Django response object, and specify content_type as pdf
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="report.pdf"'
+    # find the template and render it.
+    template = get_template(template_path)
+    html = template.render(context)
+
+    # create a pdf
+    pisa_status = pisa.CreatePDF(
+       html, dest=response, link_callback=link_callback)
+    # if error then show some funny view
+    if pisa_status.err:
+       return HttpResponse('We had some errors <pre>' + html + '</pre>')
+    return response
